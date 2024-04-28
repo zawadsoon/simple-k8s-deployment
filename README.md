3b6f577b4c05ac2d709e90277427d6394631601f
Marcin Zawada <marcin@devanddeliver.com>
2024-04-28 12:13:03 +0200

    add cert manager issuer

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # simple-k8s-deployment
 Shows some simple objects that can be used in kubernetes deployments
+
+## Notes
+
+For simplicity I will:
+    - use only default namespace
+
+See `apply.sh` file to see how apply specific objects
---
+++ apply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is an email to authorize to Let's encrypt
+LETS_ENCRYPT_EMAIL=
+
+cat cert-manager-issuer.yaml 
+    | sed "s/{{LETS_ENCRYPT_EMAIL}}/$LETS_ENCRYPT_EMAIL/g" 
+    # | kubectl apply -f - # Uncomment this line to apply the object
