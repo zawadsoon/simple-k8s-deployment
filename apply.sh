40870a89256c86706122eb267b25abcfe45e8d8e
Marcin Zawada <marcin@devanddeliver.com>
2024-04-28 12:22:37 +0200

    add delete command

--- apply.sh
+++ apply.sh
@@ -6,3 +6,5 @@ LETS_ENCRYPT_EMAIL=
 cat cert-manager-issuer.yaml \
     | sed "s/{{LETS_ENCRYPT_EMAIL}}/$LETS_ENCRYPT_EMAIL/g" \
     # | kubectl apply -f - # Uncomment this line to apply the object
+
+# Type `kubectl delete ClusterIssuer cluster-issuer` to delete cluster issuer object
